0300beb71c1aef66c696572e97c4d286f58b32d7
Brian Powell <brian.powell@completeset.com>
2016-07-07 08:40:24 -0400

    loop stuff

--- index.js
+++ index.js
@@ -23,27 +23,53 @@ var opts = {
     size: process.env.SIZE || ""  // Image size
 }
 
+var mongo_doc = {
+    term: "brown belt",
+    pages: 0,
+    complete: false
+}
+
 // get Mongo collection
 var coll = db.get("images");
 
-// get feed from Google
-gImageAPI.search( query, opts )
-    .then( function ( images ) {
 
-    // Loop over images list
-    async.eachSeries( images , function( img, cb ) {
+db.get("queries").find({complete:false}, function(err, queries) {
 
-        // Put them in DB
-        // NOTE: this does not remove duplicates or check to see if you already have the image
-        coll.insert( img, function( err, item ) {
-            debug( "Inserted item", err, item )
-            cb( err, item );
-        });
-    }, function( err ) {
-        process.exit()
-    });
+    // // Loop over terms
+    queries.forEach(function( query ) {
 
-}).catch( function( err ) {
-    debug( "Error", err );
-    process.exit();
-});
+        debug("query", query)
+
+        // Loop over pages
+        // for ( i=1; i <= 10; i++ ) {
+
+            opts.page = 1;
+
+            // get feed from Google
+            gImageAPI.search( query.query, opts )
+                .then( function ( images ) {
+
+                // Loop over images list
+                async.eachSeries( images , function( img, cb ) {
+
+                    // Put them in DB
+                    // NOTE: this does not remove duplicates or check to see if you already have the image
+                    coll.insert( img, function( err, item ) {
+                        debug( "Inserted item", err, item )
+                        cb( err, item );
+                    });
+                }, function( err ) {
+                    process.exit()
+                });
+
+            }).catch( function( err ) {
+                debug( "Error", err );
+                process.exit();
+            });
+        // }
+
+        db.get("queries").update({"_id": query._id}, {$set:{complete: true}})
+
+    })
+
+})
